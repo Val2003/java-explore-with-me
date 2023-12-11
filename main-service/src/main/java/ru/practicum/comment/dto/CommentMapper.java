14bf1b6c1dd4745c7e8643c33e9ee548b56f8a9e
Val2003 <val2003@yandex.ru>
2023-12-11 16:08:45 +0400

    3 этап features

---
+++ main-service/src/main/java/ru/practicum/comment/dto/CommentMapper.java
@@ -0,0 +1,20 @@
+package ru.practicum.comment.dto;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import ru.practicum.comment.model.Comment;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "event", ignore = true)
+    Comment newCommentDtoToComment(NewCommentDto newCommentDto);
+
+    CommentDto commentToCommentDto(Comment comment);
+}
